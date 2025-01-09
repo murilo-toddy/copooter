52c1f16510cdccedbf878935471e29d8d9b1bd9e
murilo-toddy <murilo.todao@gmail.com>
2025-01-08 22:06:07 -0300

    implement simple adder and full adder

---
+++ adder.go
@@ -0,0 +1,39 @@
+package main
+
+func NewSimpleAdder(input1, input2 *Node) (out, carry *Node, adder *CustomComponent) {
+	out, xorGate := NewXorGate(input1, input2)
+	carry, andGate := NewAndGate(input1, input2)
+	adder = NewCustomComponent(
+		"SimpleAdder",
+		[]Component{xorGate, andGate},
+		[]*Node{input1, input2},
+	)
+	return
+}
+
+func NewFullAdder(input1, input2, carryIn *Node) (out, carry *Node, adder *CustomComponent) {
+	intermediateXorOut, intermediateXorGate := NewXorGate(input1, input2)
+	out, xorGate := NewXorGate(intermediateXorOut, carryIn)
+
+	intermediateAnd1Out, intermediateAnd1Gate := NewAndGate(input1, input2)
+	intermediateAnd2Out, intermediateAnd2Gate := NewAndGate(input1, carryIn)
+	intermediateAnd3Out, intermediateAnd3Gate := NewAndGate(input2, carryIn)
+
+	intermediateOrOut, intermediateOrGate := NewOrGate(intermediateAnd1Out, intermediateAnd2Out)
+	carry, andGate := NewOrGate(intermediateOrOut, intermediateAnd3Out)
+
+	adder = NewCustomComponent(
+		"FullAdder",
+		[]Component{
+			intermediateXorGate,
+			xorGate,
+			intermediateAnd1Gate,
+			intermediateAnd2Gate,
+			intermediateAnd3Gate,
+			intermediateOrGate,
+			andGate,
+		},
+		[]*Node{input1, input2, carryIn},
+	)
+	return
+}
---
+++ adder_test.go
@@ -0,0 +1,82 @@
+package main
+
+import "testing"
+
+func TestSimpleAdder(t *testing.T) {
+	tt := []struct {
+		input1        NodeState
+		input2        NodeState
+		expectedOut   NodeState
+		expectedCarry NodeState
+	}{
+		{input1: Off, input2: Off, expectedOut: Off, expectedCarry: Off},
+		{input1: On, input2: Off, expectedOut: On, expectedCarry: Off},
+		{input1: Off, input2: On, expectedOut: On, expectedCarry: Off},
+		{input1: On, input2: On, expectedOut: Off, expectedCarry: On},
+	}
+	for _, tc := range tt {
+		components := []Component{}
+		input1 := NewNode("Input1")
+		input2 := NewNode("Input2")
+		inputComponents := []Component{
+			NewInput(input1, tc.input1),
+			NewInput(input2, tc.input2),
+		}
+		components = append(components, inputComponents...)
+
+		adderOut, adderCarry, adder := NewSimpleAdder(input1, input2)
+		components = append(components, adder)
+
+		c := NewCircuit(components, 4, false)
+		if err := c.Simulate(); err != nil {
+			t.Errorf(err.Error())
+		}
+		if adderOut.State != tc.expectedOut || adderCarry.State != tc.expectedCarry {
+			t.Errorf("Inputs<input1: %s, input2: %s> generated output state <out: %s, carry: %s> instead of <out: %s, carry: %s>",
+				tc.input1, tc.input2, adderOut.State, adderCarry.State, tc.expectedOut, tc.expectedCarry)
+		}
+	}
+}
+
+func TestFullAdder(t *testing.T) {
+	tt := []struct {
+		input1        NodeState
+		input2        NodeState
+		carryIn       NodeState
+		expectedOut   NodeState
+		expectedCarry NodeState
+	}{
+		{input1: Off, input2: Off, carryIn: Off, expectedOut: Off, expectedCarry: Off},
+		{input1: On, input2: Off, carryIn: Off, expectedOut: On, expectedCarry: Off},
+		{input1: Off, input2: On, carryIn: Off, expectedOut: On, expectedCarry: Off},
+		{input1: On, input2: On, carryIn: Off, expectedOut: Off, expectedCarry: On},
+		{input1: Off, input2: Off, carryIn: On, expectedOut: On, expectedCarry: Off},
+		{input1: On, input2: Off, carryIn: On, expectedOut: Off, expectedCarry: On},
+		{input1: Off, input2: On, carryIn: On, expectedOut: Off, expectedCarry: On},
+		{input1: On, input2: On, carryIn: On, expectedOut: On, expectedCarry: On},
+	}
+	for _, tc := range tt {
+		components := []Component{}
+		input1 := NewNode("Input1")
+		input2 := NewNode("Input2")
+		carryIn := NewNode("CarryIn")
+		inputComponents := []Component{
+			NewInput(input1, tc.input1),
+			NewInput(input2, tc.input2),
+			NewInput(carryIn, tc.carryIn),
+		}
+		components = append(components, inputComponents...)
+
+		adderOut, adderCarry, adder := NewFullAdder(input1, input2, carryIn)
+		components = append(components, adder)
+
+		c := NewCircuit(components, 4, false)
+		if err := c.Simulate(); err != nil {
+			t.Errorf(err.Error())
+		}
+		if adderOut.State != tc.expectedOut || adderCarry.State != tc.expectedCarry {
+			t.Errorf("Inputs<input1: %s, input2: %s> generated output state <out: %s, carry: %s> instead of <out: %s, carry: %s>",
+				tc.input1, tc.input2, adderOut.State, adderCarry.State, tc.expectedOut, tc.expectedCarry)
+		}
+	}
+}
