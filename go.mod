ca8c0696f30f5f633a14ef46c8fc9cc28007aac3
murilo-toddy <murilo.todao@gmail.com>
2025-01-08 22:17:49 -0300

    add raylib dependency for UI

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/murilo-toddy/copooter
 
 go 1.22.2
+
+require (
+	github.com/ebitengine/purego v0.8.1 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20241228120719-d58ffe1a3a73 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,68 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
 	components := []Component{}
-	components = append(components, BaseComponents...)
-
-	input1 := NewNode("input1")
-	input2 := NewNode("input2")
-	inputComponents := []Component{
-		NewInput(input1, Off),
-		NewInput(input2, Off),
-	}
-	components = append(components, inputComponents...)
-
-	// NOT gate
-	notOutput, notGate := NewNotGate(input1)
-	notGateComponents := []Component{
-		notGate,
-		NewMultimeter(notOutput),
-	}
-	components = append(components, notGateComponents...)
-
-	// AND gate
-	andOutput, andGate := NewAndGate(input1, input2)
-	andGateComponents := []Component{
-		andGate,
-		NewMultimeter(andOutput),
-	}
-	components = append(components, andGateComponents...)
-
-	// OR gate
-	orOutput, orGate := NewOrGate(input1, input2)
-	orGateComponents := []Component{
-		orGate,
-		NewMultimeter(orOutput),
-	}
-	components = append(components, orGateComponents...)
-
-	// NAND gate
-	nandOutput, nandGate := NewNandGate(input1, input2)
-	nandGateComponents := []Component{
-		nandGate,
-		NewMultimeter(nandOutput),
-	}
-	components = append(components, nandGateComponents...)
-
-	// XOR gate
-	xorOutput, xorGate := NewXorGate(input1, input2)
-	xorGateComponents := []Component{
-		xorGate,
-		NewMultimeter(xorOutput),
-	}
-	components = append(components, xorGateComponents...)
-
 	maxDefers := 10
 	debug := false
 	circuit := NewCircuit(components, maxDefers, debug)
-	err := circuit.Simulate()
-	if err != nil {
-		fmt.Printf("unable to run simulation: %s\n", err.Error())
-		os.Exit(1)
+	circuit.Simulate()
+
+	rl.InitWindow(800, 600, "Copooter Sim")
+	defer rl.CloseWindow()
+	rl.SetTargetFPS(60)
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.NewColor(18, 18, 18, 255))
+		rl.EndDrawing()
 	}
 }
